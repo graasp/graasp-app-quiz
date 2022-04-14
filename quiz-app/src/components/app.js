b709e823c0e2077f750ce4b25106553531c47f01
fouad <fouad.mahmoud@epfl.ch>
2022-04-14 15:29:57 +0200

    button fix

--- quiz-app/src/components/app.js
+++ quiz-app/src/components/app.js
@@ -1,5 +1,5 @@
 // import './App.css';
-import { TextField, Grid, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, spacing } from '@mui/material'
+import { TextField, Grid, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Button, spacing } from '@mui/material'
 import React, { useState } from "react"
 
 function App() {
@@ -10,12 +10,13 @@ function App() {
   const [option4, setValue5] = useState("");
   const [correctAns, setCorrectAns] = useState("");
 
+
   return (
     <div className="Quiz_App" align="center">
       <h1> Create your quiz </h1>
       <Grid container direction={"column"} spacing={2}>
         <Grid item xs={6}>
-          <p1> Enter Question 1</p1>
+          <p1> Enter Question 1:</p1>
         </Grid>
         <Grid item>
           <TextField
@@ -31,8 +32,8 @@ function App() {
         </Grid>
         <Grid item xs={6}>
           <FormControl component="fieldset">
-            <Grid item sx={{ py : 2}}>
-              <p1> Enter Choice 1</p1>
+            <Grid item sx={{ py: 2 }}>
+              <p1> Enter Choice 1:</p1>
             </Grid>
             <TextField
               value={option1}
@@ -44,12 +45,15 @@ function App() {
                 setValue2(t.target.value)
               }}
             />
-            <FormLabel component="legend">Answers: </FormLabel>
-            <RadioGroup aria-label="correct answer" name="correct answer" value={correctAns} onChange = {event => setCorrectAns(event.target.checked)}>
-              <FormControlLabel value="1" control={<Radio />} label = "correct answer"/>
+            <FormLabel component="legend" id="correct answer">Answers: </FormLabel>
+            <RadioGroup aria-labelledby="correct answer" name="correct answer" checked={correctAns} onChange={event => {
+              console.log(event.target.checked)
+              setCorrectAns(event.target.checked)
+            }}>
+              <FormControlLabel value="1" control={<Radio />} label="correct answer" />
 
-              <Grid item sx={{ py : 2}}>
-                <p2> Enter Choice 2</p2>
+              <Grid item sx={{ py: 2 }}>
+                <p2> Enter Choice 2:</p2>
               </Grid>
               <TextField
                 value={option2}
@@ -62,10 +66,10 @@ function App() {
                 }}
               />
 
-              <FormControlLabel value="2" control={<Radio />} label = "correct answer"/>
+              <FormControlLabel value="2" control={<Radio />} label="correct answer" />
 
-              <Grid item sx={{ py : 2}}>
-                <p3> Enter Choice 3</p3>
+              <Grid item sx={{ py: 2 }}>
+                <p3> Enter Choice 3:</p3>
               </Grid>
               <TextField
                 value={option3}
@@ -78,9 +82,9 @@ function App() {
                 }}
               />
 
-              <FormControlLabel value="3" control={<Radio />} label = "correct answer"/>
-              <Grid item sx={{ py : 2}}>
-                <p4> Enter Choice 4</p4>
+              <FormControlLabel value="3" control={<Radio />} label="correct answer" />
+              <Grid item sx={{ py: 2 }}>
+                <p4> Enter Choice 4:</p4>
               </Grid>
               <TextField
                 value={option4}
@@ -92,9 +96,14 @@ function App() {
                   setValue5(t.target.value)
                 }}
               />
-              <FormControlLabel value="4" control={<Radio />} label = "correct answer"/>
+              <FormControlLabel value="4" control={<Radio />} label="correct answer" />
             </RadioGroup>
           </FormControl>
+          <Grid item sx={{ py: 2, ml: 22 }}>
+            <Button variant="contained" color="success">
+              Save
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </div>
