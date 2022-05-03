ec56595892bd5163f597d5687c4dbbc8d20e6514
fouad <fouad.mahmoud@epfl.ch>
2022-05-03 19:37:23 +0200

    apply typography and other changes

--- src/components/App.js
+++ src/components/App.js
@@ -26,13 +26,13 @@ function App() {
 
   return (
     <div align="center">
-      <h1> Create your quiz </h1>
+      <Typography variant="h1" fontSize={40} sx={{ pb: 4 }}> Create your quiz </Typography>
       <Grid container direction={"column"} spacing={2} columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}>
         <Grid item xs={6} align="left">
-          <p1> Enter Question 1:</p1>
+          <Typography variant="p1"> Enter Question 1:</Typography>
         </Grid>
         <Grid item align="left">
-          
+
           <TextField
             value={question}
             placeholder="Enter Question"
@@ -45,129 +45,129 @@ function App() {
           />
         </Grid>
         <Grid item xs={6}>
-            <FormLabel component="legend" id="correct answer">Answers: </FormLabel>
-            <Grid item sx={{ py: 2 }}>
-              <Grid container direction={"row"} spacing={20}>
-                <Grid item>
-                  <Typography variant="ch1"> Enter Choice 1:</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="cr"> Correct Answer:</Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid container direction={"row"} spacing={9} alignItems="center">
+          <FormLabel component="legend" id="correct answer"> </FormLabel>
+          <Grid item sx={{ py: 2 }}>
+            <Grid container direction={"row"} spacing={20}>
               <Grid item>
-                <TextField
-                  value={choice1}
-                  placeholder="Enter Choice 1"
-                  label="Choice 1"
-                  name="Choice 1"
-                  variant="outlined"
-                  onChange={t => {
-                    setChoice1(t.target.value)
-                  }}
-                />
+                <Typography variant="ch1"> Enter Choice 1:</Typography>
               </Grid>
               <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={answer1}
-                      onChange={handleAnswerCorrectnessChange}
-                      name="answer1"
-                    />
-                  }
-                />
+                <Typography variant="cr"> Correct Answer:</Typography>
               </Grid>
             </Grid>
+          </Grid>
+          <Grid container direction={"row"} spacing={9} alignItems="center">
+            <Grid item>
+              <TextField
+                value={choice1}
+                placeholder="Enter Choice 1"
+                label="Choice 1"
+                name="Choice 1"
+                variant="outlined"
+                onChange={t => {
+                  setChoice1(t.target.value)
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={answer1}
+                    onChange={handleAnswerCorrectnessChange}
+                    name="answer1"
+                  />
+                }
+              />
+            </Grid>
+          </Grid>
 
-            <Grid item sx={{ py: 2, mr: 35 }}>
-              <Typography variant="ch2"> Enter Choice 2:</Typography>
+          <Grid item sx={{ py: 2, mr: 35 }}>
+            <Typography variant="ch2"> Enter Choice 2:</Typography>
+          </Grid>
+          <Grid container direction={"row"} spacing={9} alignItems="center">
+            <Grid item>
+              <TextField
+                value={choice2}
+                placeholder="Enter Choice 2"
+                label="Choice 2"
+                name="Choice 2"
+                variant="outlined"
+                onChange={t => {
+                  setChoice2(t.target.value)
+                }}
+              />
             </Grid>
-            <Grid container direction={"row"} spacing={9} alignItems="center">
-              <Grid item>
-                <TextField
-                  value={choice2}
-                  placeholder="Enter Choice 2"
-                  label="Choice 2"
-                  name="Choice 2"
-                  variant="outlined"
-                  onChange={t => {
-                    setChoice2(t.target.value)
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={answer2}
-                      onChange={handleAnswerCorrectnessChange}
-                      name="answer2"
-                    />
-                  }
-                />
-              </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={answer2}
+                    onChange={handleAnswerCorrectnessChange}
+                    name="answer2"
+                  />
+                }
+              />
             </Grid>
+          </Grid>
 
-            <Grid item sx={{ py: 2, mr: 35 }}>
-              <Typography variant="ch3"> Enter Choice 3:</Typography>
+          <Grid item sx={{ py: 2, mr: 35 }}>
+            <Typography variant="ch3"> Enter Choice 3:</Typography>
+          </Grid>
+          <Grid container direction={"row"} spacing={9} alignItems="center">
+            <Grid item>
+              <TextField
+                value={choice3}
+                placeholder="Enter Choice 3"
+                label="Choice 3"
+                name="Choice 3"
+                variant="outlined"
+                onChange={t => {
+                  setChoice3(t.target.value)
+                }}
+              />
             </Grid>
-            <Grid container direction={"row"} spacing={9} alignItems="center">
-              <Grid item>
-                <TextField
-                  value={choice3}
-                  placeholder="Enter Choice 3"
-                  label="Choice 3"
-                  name="Choice 3"
-                  variant="outlined"
-                  onChange={t => {
-                    setChoice3(t.target.value)
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={answer3}
-                      onChange={handleAnswerCorrectnessChange}
-                      name="answer3"
-                    />
-                  }
-                />
-              </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={answer3}
+                    onChange={handleAnswerCorrectnessChange}
+                    name="answer3"
+                  />
+                }
+              />
             </Grid>
+          </Grid>
 
-            <Grid item sx={{ py: 2, mr: 35 }}>
-              <Typography variant="ch4"> Enter Choice 4:</Typography>
+          <Grid item sx={{ py: 2, mr: 35 }}>
+            <Typography variant="ch4"> Enter Choice 4:</Typography>
+          </Grid>
+          <Grid container direction={"row"} spacing={9} alignItems="center">
+            <Grid item>
+              <TextField
+                value={choice4}
+                placeholder="Enter Choice 4"
+                label="Choice 4"
+                name="Choice 4"
+                variant="outlined"
+                onChange={t => {
+                  setChoice4(t.target.value)
+                }}
+              />
             </Grid>
-            <Grid container direction={"row"} spacing={9} alignItems="center">
-              <Grid item>
-                <TextField
-                  value={choice4}
-                  placeholder="Enter Choice 4"
-                  label="Choice 4"
-                  name="Choice 4"
-                  variant="outlined"
-                  onChange={t => {
-                    setChoice4(t.target.value)
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={answer4}
-                      onChange={handleAnswerCorrectnessChange}
-                      name="answer4"
-                    />
-                  }
-                />
-              </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={answer4}
+                    onChange={handleAnswerCorrectnessChange}
+                    name="answer4"
+                  />
+                }
+              />
             </Grid>
+          </Grid>
           <Grid item sx={{ py: 2 }}>
             <Button variant="contained" color="success">
               Save
