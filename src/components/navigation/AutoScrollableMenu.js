2a0232a3c0d9d5bb0a78f1d1d2bf9821eaaa2ba2
David <dschroeter96@gmail.com>
2023-04-07 14:48:10 +0200

    implement auto scrollable menu for table by question

---
+++ src/components/navigation/AutoScrollableMenu.js
@@ -0,0 +1,168 @@
+import { useEffect, useState } from 'react';
+
+import { Link, Stack, styled, useTheme } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+/**
+ * A component that serve as a menu, that is used to easily navigate between different element of a page,
+ * And follow the position of the user on the page to let him know where he is.
+ *
+ * @param {MutableRefObject<null>} containerRef The element to use as parent for the observer pattern
+ * @param {MutableRefObject<{}>} elemRefs A list of element stored as an object, those element will be observed
+ * for intersection with the parent element
+ * @param {{label: string, link: string}[]} links The list of the elements we want to add to this menu
+ */
+const AutoScrollableMenu = ({ containerRef, elemRefs, links }) => {
+  /**
+   * Restyled Link component to use for question navigation
+   */
+  const QuestionLink = styled(Link)({
+    color: 'black',
+    maxWidth: '150px',
+    padding: '5px',
+    textDecoration: 'none',
+    borderWidth: '2px',
+    borderRightStyle: 'solid',
+    borderColor: 'transparent',
+  });
+
+  const theme = useTheme();
+  const [highlightedLink, setHighlightedLink] = useState('');
+  const [questionVisibility, setQuestionVisibility] = useState({});
+  const [clickedItem, setClickedItem] = useState({});
+
+  /**
+   * register call back to listen for intersection with the received elements
+   */
+  useEffect(() => {
+    const observerOption = {
+      root: containerRef?.current,
+      rootMargin: '0px',
+      threshold: 0.25,
+    };
+    const intersectionCallback = (entries) => {
+      const newVisibility = entries.reduce((obj, elm) => {
+        return {
+          ...obj,
+          [elm.target.id]: elm.isIntersecting,
+        };
+      }, {});
+      setQuestionVisibility((prev) => {
+        return { ...prev, ...newVisibility };
+      });
+    };
+    const observer = new IntersectionObserver(
+      intersectionCallback,
+      observerOption
+    );
+    Object.entries(elemRefs.current).forEach(([, value]) =>
+      observer.observe(value)
+    );
+
+    // disconnect all observed value upon destruction of component
+    return () => {
+      observer.disconnect();
+    };
+  }, [elemRefs, containerRef]);
+
+  /**
+   * Subscribe to scroll event to be able to determine when we stop scrolling to set the correct clicked item
+   * if the scroll has been triggered by a click event
+   */
+  useEffect(() => {
+    // store ref, to be able to remove listener when component is destroyed
+    const ref = containerRef.current;
+    const endScrollCallback = () => {
+      setHighlightedLink(clickedItem.link);
+      setClickedItem({}); // set back the clicked item to null once set
+    };
+
+    // If an item as been clicked, 'link' property won't be undefined, and we will set a timeout
+    // to set the clicked link as clicked when scroll end (i.e. we reached the bottom)
+    // create a new timeout each 100ms as long as we keep scrolling
+    const callback = () => {
+      if (clickedItem.link !== undefined) {
+        clearTimeout(window?.scrollEndTimer);
+        window.scrollEndTimer = setTimeout(endScrollCallback, 100);
+      }
+    };
+    ref.addEventListener('scroll', callback);
+
+    // remove the listener upon destruction of component
+    return () => {
+      ref.removeEventListener('scroll', callback);
+    };
+  }, [containerRef, clickedItem]);
+
+  /**
+   * update the highlighted link whenever the question visibility changes
+   */
+  useEffect(() => {
+    if (questionVisibility !== undefined) {
+      const e = Object.entries(questionVisibility).find(([, value]) => value);
+      if (e !== undefined) {
+        setHighlightedLink(e[0]);
+      }
+    }
+  }, [questionVisibility]);
+
+  /**
+   * Helper function to set the correct style to the link given link property
+   *
+   * @param {string} link the link associated with this question
+   * @returns The additional css style to apply to thins link
+   */
+  const linkSxProp = (link) => {
+    return highlightedLink === link
+      ? {
+          borderColor: theme.palette.primary.main,
+          '&:hover': {
+            color: theme.palette.primary.main,
+          },
+        }
+      : {
+          borderColor: 'transparent',
+          '&:hover': {
+            color: '#878383',
+            borderColor: '#878383',
+          },
+        };
+  };
+
+  /**
+   * Helper function to handle the click on a link
+   *
+   * We already start a timeout upon clicking, to handle scenario where no scroll is required to
+   * reach an element, to be sure that the element we clicked on appears as selected
+   *
+   * @param {string} link The label of the question we clicked on
+   */
+  const handleLinkClicked = (link) => {
+    setClickedItem({ link });
+    const clickCallback = () => {
+      setHighlightedLink(link);
+      setClickedItem({}); // set back the clicked item to null once set
+    };
+
+    window.scrollEndTimer = setTimeout(clickCallback, 100);
+  };
+
+  return (
+    <Stack>
+      {links.map(({ label, link }) => {
+        return (
+          <QuestionLink
+            key={label}
+            href={`#${link}`}
+            sx={() => linkSxProp(link)}
+            onClick={() => handleLinkClicked(link)}
+          >
+            <Typography>{label}</Typography>
+          </QuestionLink>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default AutoScrollableMenu;
--- src/components/results/ResultTables.js
+++ src/components/results/ResultTables.js
@@ -1,24 +1,102 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  useSyncExternalStore,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Stack } from '@mui/material';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 import { hooks } from '../../config/queryClient';
 import { TABLE_BY_QUESTION_CONTAINER_CY } from '../../config/selectors';
 import { QuizContext } from '../context/QuizContext';
-import { getAllAppDataByQuestionId, getDataWithId } from '../context/utilities';
+import { getAllAppDataByQuestionId } from '../context/utilities';
+import AutoScrollableMenu from '../navigation/AutoScrollableMenu';
 import TableByQuestion from './TableByQuestion';
 
 /**
+ * Component that represents the result tables
  *
- * @returns {JSX.Element}
- * @constructor
+ * @param headerElem The element that represents the header, if no header, simply let it to undefined
+ * this header is used to calculate the space left for the body of this component
  */
-const ResultTables = () => {
+const ResultTables = ({ headerElem }) => {
   const { t } = useTranslation();
   const { data: responses, isLoading } = hooks.useAppData();
   const { order, questions } = useContext(QuizContext);
+  const questionContainerRef = useRef(null);
+  const questionRefs = useRef({});
+
+  /**
+   * Helper function to extract the data from the questions
+   *
+   * add a property `innerLink` that represents the link to use as hash link
+   */
+  const extractQuestionData = useCallback(() => {
+    return questions
+      .groupBy((q) => q.id)
+      .map((e) => {
+        return e.map((el) => {
+          return {
+            ...el.data,
+            innerLink: el.data.question.replaceAll(' ', '-'),
+          };
+        });
+      });
+  }, [questions]);
+
+  const [questionData, setQuestionData] = useState(extractQuestionData());
+
+  /**
+   * Helper function to calculate the full height of an element, take margin into account
+   */
+  const getElemFullHeight = useCallback((elem) => {
+    const elemStyle = getComputedStyle(elem);
+    return (
+      elem.offsetHeight +
+      parseInt(elemStyle.marginTop) +
+      parseInt(elemStyle.marginBottom)
+    );
+  }, []);
+
+  /**
+   * Stores the height of the header, dynamically updated if it is resized
+   */
+  const headerHeight = useSyncExternalStore(
+    (callback) => {
+      headerElem?.current.addEventListener('resize', callback);
+      return () => headerElem?.current.removeEventListener('resize', callback);
+    },
+    () => {
+      return getElemFullHeight(headerElem?.current ?? 0);
+    }
+  );
+
+  /**
+   * Stores the height of the viewport, dynamically updated if it is resized
+   */
+  const windowHeight = useSyncExternalStore(
+    (callback) => {
+      window.addEventListener('resize', callback);
+      return () => window.removeEventListener('resize', callback);
+    },
+    () => {
+      return window.innerHeight;
+    }
+  );
+
+  /**
+   * Helper function that returns the max height that the displayed component can have
+   * to not overflow out of the window.
+   */
+  const calcMaxHeight = useCallback(() => {
+    return windowHeight - headerHeight - 16; // 16 is just empirically determined to prevent the page scroll bar to display
+  }, [windowHeight, headerHeight]);
 
   /**
    * Helper function to get the name of all users that have answered to at least one question.
@@ -35,19 +113,51 @@ const ResultTables = () => {
     setUsers(getAllUsers());
   }, [responses, getAllUsers]);
 
+  useEffect(() => {
+    setQuestionData(extractQuestionData());
+  }, [questions, extractQuestionData]);
+
   if (isLoading) {
     return <CircularProgress />;
   }
 
   return order.length > 0 ? (
-    order.map((qId) => (
-      <TableByQuestion
-        key={qId}
-        userList={users}
-        question={{ id: qId, data: getDataWithId(questions, qId).data }}
-        responses={getAllAppDataByQuestionId(responses, qId)}
-      />
-    ))
+    <Stack direction="row" spacing={5}>
+      <Box sx={{ maxHeight: calcMaxHeight(), overflow: 'auto' }}>
+        <AutoScrollableMenu
+          links={order.map((qId) => {
+            const data = questionData.get(qId).first();
+            return { label: data.question, link: data.innerLink };
+          })}
+          elemRefs={questionRefs}
+          containerRef={questionContainerRef}
+        />
+      </Box>
+      <Box
+        sx={{
+          overflow: 'auto',
+          width: '100%',
+          height: calcMaxHeight(),
+          pr: 1,
+          scrollBehavior: 'smooth',
+        }}
+        ref={questionContainerRef}
+      >
+        {order.map((qId) => (
+          <Box
+            key={qId}
+            id={questionData?.get(qId).first().innerLink}
+            ref={(elm) => (questionRefs.current[qId] = elm)}
+          >
+            <TableByQuestion
+              userList={users}
+              question={{ id: qId, data: questionData?.get(qId).first() }}
+              responses={getAllAppDataByQuestionId(responses, qId)}
+            />
+          </Box>
+        ))}
+      </Box>
+    </Stack>
   ) : (
     <Typography align="center" data-cy={TABLE_BY_QUESTION_CONTAINER_CY}>
       {t("There isn't any questions to display")}
