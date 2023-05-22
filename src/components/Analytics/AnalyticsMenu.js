e82fe49563e73c019b15cb0fa29c1d841b2ea73a
David <dschroeter96@gmail.com>
2023-05-22 12:47:25 +0200

    git fix small bug in analitics menu, to display scroll bar when needed

--- src/components/Analytics/AnalyticsMenu.js
+++ src/components/Analytics/AnalyticsMenu.js
@@ -50,7 +50,7 @@ const AnalyticsMenu = ({ headerElem }) => {
       <Stack direction="row" ref={stackElemRef}>
         <Box
           ref={sideMenuElemRef}
-          sx={{ minWidth: '8em', maxHeight: maxResultViewHeight }}
+          sx={{ minWidth: '8em', maxHeight: maxResultViewHeight, overflow: 'auto' }}
         >
           <AutoScrollableMenu
             links={menuLabels}
