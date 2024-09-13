129757c9cd4b06ee8410685f634057e3dd9c0963
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-09-13 09:15:49 +0200

    feat: improve the multiple choices player view * feat: remove the reorder animation * feat: display hint only on wrong selected answers * feat: display error on correction and correctness

--- src/components/play/PlayViewQuestionType.tsx
+++ src/components/play/PlayViewQuestionType.tsx
@@ -97,7 +97,6 @@ export const PlayViewQuestionType = ({
           }
           showCorrection={showCorrection}
           showCorrectness={showCorrectness}
-          numberOfSubmit={numberOfSubmit}
           numberOfRetry={numberOfRetry}
         />
       );
