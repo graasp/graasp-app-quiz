db9d39aeafb92b2e8a42f06decc111f2ca884efc
kim <kim.phanhoang@epfl.ch>
2024-12-13 16:21:48 +0100

    refactor: retry test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    retries: {
+      runMode: 2,
+    },
     setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('@cypress/code-coverage/task')(on, config);
