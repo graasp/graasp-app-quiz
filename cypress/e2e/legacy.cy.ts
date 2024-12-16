717c14de0e817bf14e07b7b06c26b1a1130c6f02
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-12-16 09:08:05 +0100

    fix: prevent infinite loop for readers and network errors on legacy data (#187)
    * fix: prevent infinite loop for readers and network errors on legacy data

--- cypress/e2e/legacy.cy.ts
+++ cypress/e2e/legacy.cy.ts
@@ -1,7 +1,8 @@
-import { AppSetting, Context } from '@graasp/sdk';
+import { AppSetting, Context, PermissionLevel } from '@graasp/sdk';
 
 import { APP_SETTING_NAMES, QuestionType } from '../../src/config/constants';
 import {
+  CREATE_QUESTION_TITLE_CY,
   PLAY_VIEW_QUESTION_TITLE_CY,
   QUESTION_BAR_NEXT_CY,
   buildPlayViewTextInputCy,
@@ -11,88 +12,89 @@ import { datesFactory, mockAppDataFactory } from '../../src/data/factories';
 import { mockItem } from '../../src/data/items';
 import { mockCurrentMember } from '../../src/data/members';
 
+const MULTIPLE_CHOICES_APP_SETTING: AppSetting = {
+  id: 'multiple-choice-id',
+  name: APP_SETTING_NAMES.QUESTION,
+  item: mockItem,
+  ...datesFactory,
+  data: {
+    type: QuestionType.MULTIPLE_CHOICES,
+    // does not have a question id
+    question: 'My multiple choice question',
+    choices: [
+      {
+        value: 'choice 1',
+        isCorrect: false,
+      },
+      {
+        value: 'choice 2',
+        isCorrect: true,
+      },
+      {
+        value: 'choice 3',
+        isCorrect: true,
+      },
+      {
+        value: 'choice 4',
+        isCorrect: false,
+      },
+    ],
+    explanation: 'my explanation for multiple choice',
+  },
+};
+
+const TEXT_INPUT_APP_SETTING: AppSetting = {
+  id: 'text-input-id',
+  name: APP_SETTING_NAMES.QUESTION,
+  item: mockItem,
+  ...datesFactory,
+  data: {
+    type: QuestionType.TEXT_INPUT,
+    // does not have a question id
+    question: 'My text input question',
+    text: 'my text input',
+    explanation: 'my explanation for text input',
+  },
+};
+
+const ORDER_LIST: AppSetting = {
+  id: 'order-list',
+  name: APP_SETTING_NAMES.QUESTION_LIST,
+  item: mockItem,
+  ...datesFactory,
+  data: {
+    // refer to real question id
+    list: [MULTIPLE_CHOICES_APP_SETTING.id, TEXT_INPUT_APP_SETTING.id],
+  },
+};
+
+// current user
+const textAppData = mockAppDataFactory({
+  item: mockItem,
+  creator: mockCurrentMember,
+  data: {
+    // refer to real question id
+    questionId: TEXT_INPUT_APP_SETTING.id,
+    text: '90',
+  },
+  id: '3',
+});
+const appData = [
+  mockAppDataFactory({
+    item: mockItem,
+    creator: mockCurrentMember,
+    data: {
+      // refer to real question id
+      questionId: MULTIPLE_CHOICES_APP_SETTING.id,
+      choices: ['Paris'],
+    },
+    id: '2',
+  }),
+  textAppData,
+];
+
 describe('Legacy', () => {
   it('Questions without questionId are compatible', () => {
-    const MULTIPLE_CHOICES_APP_SETTING: AppSetting = {
-      id: 'multiple-choice-id',
-      name: APP_SETTING_NAMES.QUESTION,
-      item: mockItem,
-      ...datesFactory,
-      data: {
-        type: QuestionType.MULTIPLE_CHOICES,
-        // does not have a question id
-        question: 'My multiple choice question',
-        choices: [
-          {
-            value: 'choice 1',
-            isCorrect: false,
-          },
-          {
-            value: 'choice 2',
-            isCorrect: true,
-          },
-          {
-            value: 'choice 3',
-            isCorrect: true,
-          },
-          {
-            value: 'choice 4',
-            isCorrect: false,
-          },
-        ],
-        explanation: 'my explanation for multiple choice',
-      },
-    };
-
-    const TEXT_INPUT_APP_SETTING: AppSetting = {
-      id: 'text-input-id',
-      name: APP_SETTING_NAMES.QUESTION,
-      item: mockItem,
-      ...datesFactory,
-      data: {
-        type: QuestionType.TEXT_INPUT,
-        // does not have a question id
-        question: 'My text input question',
-        text: 'my text input',
-        explanation: 'my explanation for text input',
-      },
-    };
-
-    const ORDER_LIST: AppSetting = {
-      id: 'order-list',
-      name: APP_SETTING_NAMES.QUESTION_LIST,
-      item: mockItem,
-      ...datesFactory,
-      data: {
-        // refer to real question id
-        list: [MULTIPLE_CHOICES_APP_SETTING.id, TEXT_INPUT_APP_SETTING.id],
-      },
-    };
-
-    // current user
-    const textAppData = mockAppDataFactory({
-      item: mockItem,
-      creator: mockCurrentMember,
-      data: {
-        // refer to real question id
-        questionId: TEXT_INPUT_APP_SETTING.id,
-        text: '90',
-      },
-      id: '3',
-    });
-    const appData = [
-      mockAppDataFactory({
-        item: mockItem,
-        creator: mockCurrentMember,
-        data: {
-          // refer to real question id
-          questionId: MULTIPLE_CHOICES_APP_SETTING.id,
-          choices: ['Paris'],
-        },
-        id: '2',
-      }),
-      textAppData,
-    ];
     cy.setUpApi({
       database: {
         appSettings: [
@@ -104,24 +106,34 @@ describe('Legacy', () => {
       },
       appContext: {
         context: Context.Builder,
+        permission: PermissionLevel.Admin,
       },
     });
     cy.visit('/');
 
-    cy.get(`${dataCyWrapper(PLAY_VIEW_QUESTION_TITLE_CY)}`).should(
-      'contain',
+    cy.get(`${dataCyWrapper(CREATE_QUESTION_TITLE_CY)} input`).should(
+      'have.value',
       MULTIPLE_CHOICES_APP_SETTING.data.question
     );
-    // go to next and check text input
-    cy.get(dataCyWrapper(QUESTION_BAR_NEXT_CY)).click();
-    cy.get(`${dataCyWrapper(PLAY_VIEW_QUESTION_TITLE_CY)}`).should(
-      'contain',
-      TEXT_INPUT_APP_SETTING.data.question
-    );
+  });
 
-    cy.get(`${dataCyWrapper(buildPlayViewTextInputCy(false))} input`).should(
-      'have.value',
-      textAppData.data.text
-    );
+  it('Show error banner if missing questionId for readers', () => {
+    cy.setUpApi({
+      database: {
+        appSettings: [
+          MULTIPLE_CHOICES_APP_SETTING,
+          TEXT_INPUT_APP_SETTING,
+          ORDER_LIST,
+        ],
+        appData,
+      },
+      appContext: {
+        context: Context.Builder,
+        permission: PermissionLevel.Read,
+      },
+    });
+    cy.visit('/');
+
+    cy.get(`[role="alert"]`).should('be.visible');
   });
 });
